193c173be1e5099cc2271f25cd5ec42e0cd958d0
jonaskris <jo_nas@live.no>
2017-09-13 13:40:02 +0200

    dseff

--- Oeving 3/Oeving3.java
+++ Oeving 3/Oeving3.java
@@ -14,6 +14,9 @@ import lejos.hardware.Keys;
 import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 import lejos.hardware.sensor.*;
+import lejos.hardware.Device;
+import java.io.Closeable;
+import lejos.hardware.Button;
 
 
 /* Pseudo kode
@@ -29,55 +32,39 @@ import lejos.hardware.sensor.*;
 */
 
 public class Oeving3 {
-public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception{
 
-	boolean sluttenAvTunellen = false;
-	boolean erDetNoenBiler = false;
-	final long stoppeVarighet = 3000;	// Hvor lenge skal bilen stoppe naar den moeter en bil. (Oppgis i ms)
-	final double lydTerskel = 0.6; 		// Hvor hoey lyd maa noe lage for at det skal gjenkjennes som en bil.
-	double fargeTerskel = 0.0;	// Hvor lav RGB verdi maa bakken voere for at det skal gjenkjennes som svart.
-	final int motorHastighet = 200;
+		boolean sluttenAvTunellen = false;
+		boolean erDetNoenBiler = false;
+		final long stoppeVarighet = 3000;	// Hvor lenge skal bilen stoppe naar den moeter en bil. (Oppgis i ms)
+		final double lydTerskel = 0.6; 		// Hvor hoey lyd maa noe lage for at det skal gjenkjennes som en bil.
+		final double fargeTerskel = 0.0;	// Hvor lav RGB verdi maa bakken vaere for at det skal gjenkjennes som svart.
+		final int motorBHastighet = 200;
+		final int motorCHastighet = 200;
 
-	Brick brick = BrickFinder.getDefault();
-	Port s1 = brick.getPort("S1"); 		// Fargesensor
-	Port s2 = brick.getPort("S2"); 		// Lydsensor
-
-
-	EV3 ev3 = (EV3) BrickFinder.getLocal();
-	TextLCD lcd = ev3.getTextLCD();
-	Keys keys = ev3.getKeys();
-
-	Motor.B.setSpeed(-motorHastighet);
-	Motor.C.setSpeed(motorHastighet);
-
-	/* Definerer en fargesensor og fargeAvleser */
-	EV3ColorSensor fargesensor = new EV3ColorSensor(s1); // ev3-fargesensor
-    SampleProvider fargeLeser = fargesensor.getMode("RGB");  // svart = 0.01..
-    float[] fargeSample = new float[fargeLeser.sampleSize()]; // tabell som inneholder avlest verdi
-
-    /* Definerer en lydsensor og lydAvleser */
-	NXTSoundSensor lydsensor = new NXTSoundSensor(s2); // NXT-lydsensor
-	SampleProvider lydLeser = lydsensor.getDBAMode();  //
-    float[] lydSample = new float[lydLeser.sampleSize()]; // tabell som inneholder avlest verdi
+		Brick brick = BrickFinder.getDefault();
+		Port s1 = brick.getPort("S1"); 		// Fargesensor
+		Port s2 = brick.getPort("S2"); 		// Lydsensor
 
 
-	//Kode for aa definere fargeTerskel (Fargesensor maa se paa det som defineres som svart, naar denne koden kjoeres)
-	fargeLeser.fetchSample(fargeSample, 0);
-
-	lcd.drawString("Foer vasking maa fargesensor", 0,1);
-	lcd.drawString("plasseres paa sort linje", 0,2);
-	lcd.drawString("saa svart kan defineres", 0,3);
-	lcd.drawString("trykk paa en knapp for aa", 0,4);
-	lcd.drawString("starte kalibrering.", 0,5);
-	keys.waitForAnyPress();
-	LCD.clear();
-	fargeTerskel = fargeSample[0];
-	lcd.drawString("Kalibrering ferdig!", 0,1);
-	lcd.drawString("Trykk en knapp for aa starte vasking.", 0,2);
-	keys.waitForAnyPress();
+		EV3 ev3 = (EV3) BrickFinder.getLocal();
+		TextLCD lcd = ev3.getTextLCD();
+		Keys keys = ev3.getKeys();
 
 
-	// Kode som kjoerer under drift
+		Motor.B.setSpeed(motorBHastighet);
+		Motor.C.setSpeed(-motorCHastighet);
+
+		/* Definerer en fargesensor og fargeAvleser */
+		EV3ColorSensor fargesensor = new EV3ColorSensor(s1); // ev3-fargesensor
+		SampleProvider fargeLeser = fargesensor.getMode("RGB");  // svart = 0.01..
+		float[] fargeSample = new float[fargeLeser.sampleSize()]; // tabell som inneholder avlest verdi
+
+		/* Definerer en lydsensor og lydAvleser */
+		NXTSoundSensor lydsensor = new NXTSoundSensor(s2); // NXT-lydsensor
+		SampleProvider lydLeser = lydsensor.getDBAMode();  //
+		float[] lydSample = new float[lydLeser.sampleSize()]; // tabell som inneholder avlest verdi
+
 		while (true){
 			sluttenAvTunellen = false;
 			erDetNoenBiler = false;
@@ -106,14 +93,17 @@ public static void main(String[] args) throws Exception{
 				Thread.sleep(200);
 			}
 
+
 			LCD.clear();
 			lcd.drawString("Kommet til slutten av tunellen!", 0,1);
-			lcd.drawString("Trykk en knapp for aa gjenoppta vasking", 0,2);
-
 			Motor.B.stop(true);
 			Motor.C.stop(true);
-			keys.waitForAnyPress();
-			LCD.clear();
+			if(Button.readButtons() == ){
+				break;
+			}
+
 		}
+	fargesensor.close();
 	}
+
 }
