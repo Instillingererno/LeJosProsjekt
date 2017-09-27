246d0ceee7beb5e011975d3740c448b0d3b03853
Instillingererno <sveinungskole@gmail.com>
2017-09-27 11:26:49 +0200

    dtfg

--- FolgLinje/Main.java
+++ FolgLinje/Main.java
@@ -28,68 +28,54 @@ class main {
 
 	public static void main(String[] args) throws Exception {
 
-		Move unit = new Move(); // Init move object
 		int buttons = 0; // Knapper som er trykket
 		boolean cont = true; //Fortsett eller ikke
+
+		//INIT
+		//Variabler
+		final double lightFloorS1 = 0.32;	//0.50			// Hvor lav RGB verdi maa bakken vaere for at det skal gjenkjennes som svart.
+		final double colorFloorS4 = 0.02; //0.15			// Hvor lav RGB verdi maa bakken vaere for at det skal gjenkjennes som svart.
+		float backwardSpeed = 100;
+
+		Brick brick = BrickFinder.getDefault();
+		Port s1 = brick.getPort("S1"); 				// LysSensor
+		Port s4 = brick.getPort("S4"); 				// Fargesensor
+
+		/* Definerer en lyssensor og lysAvleser */
+		NXTLightSensor lysSensorS1 = new NXTLightSensor(s1);
+		SampleProvider lysLeserS1 = lysSensorS1.getRedMode();
+		float[] lysSampleS1 = new float[lysLeserS1.sampleSize()];
+
+		/* Definerer en fargesensor og fargeAvleser */
+		EV3ColorSensor fargesensorS4 = new EV3ColorSensor(s4);	 		// Fargesensor2
+		SampleProvider fargeLeserS4 = fargesensorS4.getMode("RGB");  	//
+		float[] fargeSampleS4 = new float[fargeLeserS4.sampleSize()];	//
+
+		//MOVE
+		float turnDelta = 50;
+		long startTime = System.nanoTime(); // System.nanoTime();
+		long deltaTime = 0;
+		int multiplier = 1;
+		boolean svart = false;
 
 		while(cont) {
 			buttons = Button.readButtons();
 			if(Integer.toString(buttons).contains("2")) {
 				cont = false;
 			}
-			unit.forward();
-
-		}
-		unit.exit();
-	}
-}
-class Init { // Initializing
-
-	//Variabler
-	final double lightFloorS1 = 0.32;	//0.50			// Hvor lav RGB verdi maa bakken vaere for at det skal gjenkjennes som svart.
-	final double colorFloorS4 = 0.02; //0.15			// Hvor lav RGB verdi maa bakken vaere for at det skal gjenkjennes som svart.
-	float backwardSpeed = 100;
-
-	Brick brick = BrickFinder.getDefault();
-	Port s1 = brick.getPort("S1"); 				// LysSensor
-	Port s4 = brick.getPort("S4"); 				// Fargesensor
-
-	/* Definerer en lyssensor og lysAvleser */
-	NXTLightSensor lysSensorS1 = new NXTLightSensor(s1);
-	SampleProvider lysLeserS1 = lysSensorS1.getRedMode();
-	float[] lysSampleS1 = new float[lysLeserS1.sampleSize()];
-
-	/* Definerer en fargesensor og fargeAvleser */
-	EV3ColorSensor fargesensorS4 = new EV3ColorSensor(s4);	 		// Fargesensor2
-	SampleProvider fargeLeserS4 = fargesensorS4.getMode("RGB");  	//
-	float[] fargeSampleS4 = new float[fargeLeserS4.sampleSize()];	//
-
-	public void exit() {
-		lysSensorS1.close();	//Maa inkluderes saa sensorporter lukkes etter hver programkjoering.
-		fargesensorS4.close();	//
-	}
-}
-
-class Move extends Init { // Movement
-	float turnDelta = 0;
-	long startTime = 0; // System.nanoTime();
-	long deltaTime = 0;
-	boolean svart = false;
-	byte multiplier = 1;
-	public void forward() {
-		Motor.C.setSpeed(backwardSpeed + turnDelta * multiplier);
-		Motor.D.setSpeed(-(backwardSpeed + turnDelta * multiplier));
-		Motor.C.backward();
-		Motor.D.backward();
-		fargeLeserS4.fetchSample(fargeSampleS4, 0);
-		deltaTime = startTime - System.nanoTime();
-		turnDelta = (deltaTime * deltaTime) / 10000000;
-		if(fargeSampleS4[0] < 0.7) {
-			if(svart == false) {
+			Motor.C.setSpeed(backwardSpeed + turnDelta * multiplier);
+			Motor.D.setSpeed(backwardSpeed - turnDelta * multiplier);
+			Motor.C.backward();
+			Motor.D.backward();
+			fargeLeserS4.fetchSample(fargeSampleS4, 0);
+			lysLeserS1.fetchSample(lysSampleS1, 0);
+			if(fargeSampleS4[0] < colorFloorS4) {
 				multiplier = -1;
-				startTime = System.nanoTime();
-				svart = true;
+			} else if(lysSampleS1[0] < lightFloorS1) {
+				multiplier = 1;
 			}
 		}
+		lysSensorS1.close();	//Maa inkluderes saa sensorporter lukkes etter hver programkjoering.
+		fargesensorS4.close();
 	}
 }
