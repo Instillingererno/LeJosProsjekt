a3088a5c2345855ee86dae437a0d209c220f46a9
Legend K-ern <31963457+cannabuzftw@users.noreply.github.com>
2017-11-17 15:41:20 +0100

    kek

--- LejosPiano/src/LejosPiano.java
+++ LejosPiano/src/LejosPiano.java
@@ -1,50 +1,45 @@
 import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.Sound;
+
+import java.io.File;
 
 public class LejosPiano {
     public static void main(String[] args) {
         EV3 ev3 = (EV3) BrickFinder.getLocal();
         TextLCD LCD = ev3.getTextLCD();
-        demThreads thread1 = new demThreads("Thread 1", 0);
+        int buttons = 0;
 
-        demThreads thread2 = new demThreads("Thread 2", 10000);
-        thread1.start();
-        thread2.start();
+        while(true) {
+            buttons = Button.readButtons();
+            if(Integer.toString(buttons).equals("2")) {
+               demThreads newThread = new demThreads("Sphee");
+               newThread.start();
+            }
+
+
+        }
     }
 }
 
 class demThreads extends Thread {
     private Thread thread;
     private String name;
-    private int wait;
 
-    public demThreads(String name, int wait) {
+    public demThreads(String name) {
         this.name = name;
-        this.wait = wait;
         LCD.drawString("Creating " + name, 0, 1, false);
     }
 
     public void run () {
-		try{
-			Thread.sleep(wait);
-		} catch(Exception e) {
-			//idfk
-			LCD.drawString("wtf happened", 0, 1, true);
-		}
-
-        LCD.drawString ("Running" + name, 0, 2, false);
-        try {
-            for (int i = 0; i < 4; i++) {
-                LCD.drawString("Thread" + name + ", " + i, 0, 3, false);
-                Thread.sleep(10000);
-            }
-        }
-        catch (InterruptedException e) {
-            LCD.drawString ("Thread" + name + " is interrupted.", 0, 4, false);
-        }
-        LCD.drawString ("Thread" + name + " is exiting.", 0, 5, false);
+        File a = new File("A.wav");
+       /* File c = new File("C.wav");
+        File f = new File("F.wav");
+        File g = new File("G.wav");*/
+        Sound.playSample(a);
     }
 
     public void start() {
