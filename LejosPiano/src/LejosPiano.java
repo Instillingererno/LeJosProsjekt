dc545bd9d9cd81679f75dc43797531290e7163b9
Instillingererno <sveinungskole@gmail.com>
2017-11-17 13:03:41 +0100

    sdfg

--- LejosPiano/src/LejosPiano.java
+++ LejosPiano/src/LejosPiano.java
@@ -1,5 +1,3 @@
-package Øving15;
-
 import lejos.hardware.BrickFinder;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.LCD;
@@ -9,8 +7,9 @@ public class LejosPiano {
     public static void main(String[] args) {
         EV3 ev3 = (EV3) BrickFinder.getLocal();
         TextLCD LCD = ev3.getTextLCD();
-        demThreads thread1 = new demThreads("Thread 1");
-        demThreads thread2 = new demThreads("Thread 2");
+        demThreads thread1 = new demThreads("Thread 1", 0);
+
+        demThreads thread2 = new demThreads("Thread 2", 10000);
         thread1.start();
         thread2.start();
     }
@@ -19,28 +18,37 @@ public class LejosPiano {
 class demThreads extends Thread {
     private Thread thread;
     private String name;
+    private int wait;
 
-    public demThreads(String name) {
+    public demThreads(String name, int wait) {
         this.name = name;
-        LCD.drawString("Creating " + name, 50, 50, false);
+        this.wait = wait;
+        LCD.drawString("Creating " + name, 0, 1, false);
     }
 
     public void run () {
-        LCD.drawString ("Running" + name, 50, 50, false);
+		try{
+			Thread.sleep(wait);
+		} catch(Exception e) {
+			//idfk
+			LCD.drawString("wtf happened", 0, 1, true);
+		}
+
+        LCD.drawString ("Running" + name, 0, 2, false);
         try {
             for (int i = 0; i < 4; i++) {
-                LCD.drawString("Thread" + name + ", " + i, 50, 50, false);
-                Thread.sleep(100);
+                LCD.drawString("Thread" + name + ", " + i, 0, 3, false);
+                Thread.sleep(10000);
             }
         }
         catch (InterruptedException e) {
-            LCD.drawString ("Thread" + name + " is interrupted.", 50, 50, false);
+            LCD.drawString ("Thread" + name + " is interrupted.", 0, 4, false);
         }
-        LCD.drawString ("Thread" + name + " is exiting.", 50, 50, false);
+        LCD.drawString ("Thread" + name + " is exiting.", 0, 5, false);
     }
 
     public void start() {
-        LCD.drawString("Starting" + name, 50, 50, false);
+        LCD.drawString("Starting" + name, 0, 6, false);
         if (thread == null) {
             thread = new Thread(this, name);
             thread.start();
