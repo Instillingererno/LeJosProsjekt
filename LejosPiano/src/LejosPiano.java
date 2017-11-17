0fc65ee0c962de2cb58fa0501bc92578e4f8ab3b
Instillingererno <sveinungskole@gmail.com>
2017-11-17 22:14:39 +0100

    wqrgs

--- LejosPiano/src/LejosPiano.java
+++ LejosPiano/src/LejosPiano.java
@@ -10,24 +10,53 @@ import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.NXTTouchSensor;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 
 enum Type {
     NXT, EV3
 }
 
-
 public class LejosPiano {
     public static void main(String[] args) {
         EV3 ev3 = (EV3) BrickFinder.getLocal();
         TextLCD LCD = ev3.getTextLCD();
         int buttons = 0;
-        TrykkSensor ev3Sensor1 = new TrykkSensor(Type.EV3, SensorPort.S1, "Sphee");
+        int teller = 0;
+        TrykkSensor A = new TrykkSensor(Type.EV3, SensorPort.S1, "A.wav");
+        TrykkSensor C = new TrykkSensor(Type.NXT, SensorPort.S2, "C.wav");
+        TrykkSensor F = new TrykkSensor(Type.EV3, SensorPort.S3, "F.wav");
+        TrykkSensor G = new TrykkSensor(Type.NXT, SensorPort.S4, "G.wav");
 
-        while(true) {
-            if(ev3Sensor1.isPressed()) {
-                LCD.drawString("It is pressed!", 0, 0);
+        Server host = new Server();
+        host.connect();
+
+        while(!Button.ESCAPE.isDown()) {
+
+            if(A.isPressed()) {
+                host.send(A.toString());
+                teller++;
+                LCD.drawString(Integer.toString(teller),0,1);
+            } else if(C.isPressed()) {
+                host.send(C.toString());
+                teller++;
+                LCD.drawString(Integer.toString(teller),0,1);
+            } else if(F.isPressed()) {
+                host.send(F.toString());
+                teller++;
+                LCD.drawString(Integer.toString(teller),0,1);
+            } else if(G.isPressed()) {
+                host.send(G.toString());
+                teller++;
+                LCD.drawString(Integer.toString(teller),0,1);
             }
+        }
+    }
+    public static void vent() {
+        try{
+            TimeUnit.SECONDS.sleep(5);
+        } catch(Exception e) {
+            System.out.println(e);
         }
     }
 }
@@ -62,7 +91,8 @@ class TrykkSensor {
     private Port port;
     private String output;
     private Type type;
-    private Object sensor;
+    private NXTTouchSensor nxt;
+    private EV3TouchSensor ev3;
     private boolean isLifted = true;
     float[] sample;
 
@@ -72,23 +102,29 @@ class TrykkSensor {
         this.port = port;
 
         if(this.type == Type.NXT) {
-            this.sensor = new NXTTouchSensor(this.port);
+            this.nxt = new NXTTouchSensor(this.port);
         } else if(this.type == Type.EV3) {
-            this.sensor = new EV3TouchSensor(this.port);
+            this.ev3 = new EV3TouchSensor(this.port);
         }
 
-        sample = new float[1];
+        this.sample = (this.type == Type.EV3) ? new float[ev3.sampleSize()] : new float[nxt.sampleSize()];
 
     }
 
+    public String toString() {
+        return output;
+    }
 
 
     public boolean isPressed() {
-        if (isLifted && sensor.fetchSample(sample, 0) == 1) {
+        if(this.type == Type.EV3) { ev3.fetchSample(sample,0); }
+        else { nxt.fetchSample(sample,0); }
+
+        if (isLifted && sample[0] == 1) {
             isLifted = false;
             return true;
         }
-        else if(!isLifted && sensor.fetchSample(sample, 0) == 1){
+        else if(!isLifted && sample[0] == 1){
             return false;
         }
         else {
