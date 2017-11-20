9256e80b9cb790160fc4cc9c023a85ccd12da0ad
Instillingererno <sveinungskole@gmail.com>
2017-11-20 14:18:46 +0100

    dfgh

--- LejosPiano/MidiTest/src/Pc.java
+++ LejosPiano/MidiTest/src/Pc.java
@@ -1,17 +1,19 @@
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.swing.*;
+import org.jfugue.realtime.RealtimePlayer;
+import org.jfugue.theory.Chord;
+
+import javax.sound.midi.MidiUnavailableException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Pc {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, MidiUnavailableException {
         Socket MyClient;
         DataInputStream in = null;
         DataOutputStream out = null;
+        Chord[] chords = {new Chord("E"), new Chord("B"), new Chord("C"), new Chord("A")};
+        RealtimePlayer player = new RealtimePlayer();
 
         try {
             System.out.println("Prøver tilkobling");
@@ -25,6 +27,32 @@ public class Pc {
         while (true) {
             int input = in.readInt();
             //playsound
+            switch (input) {
+                case 1:
+                    player.startChord(chords[0]);
+                    break;
+                case 2:
+                    player.stopChord(chords[0]);
+                    break;
+                case 3:
+                    player.startChord(chords[1]);
+                    break;
+                case 4:
+                    player.stopChord(chords[1]);
+                    break;
+                case 5:
+                    player.startChord(chords[2]);
+                    break;
+                case 6:
+                    player.stopChord(chords[2]);
+                    break;
+                case 7:
+                    player.startChord(chords[3]);
+                    break;
+                case 8:
+                    player.stopChord(chords[3]);
+                    break;
+            }
         }
     }
 }
--- LejosPiano/src/LejosPiano.java
+++ LejosPiano/src/LejosPiano.java
@@ -1,16 +1,14 @@
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
 import lejos.hardware.ev3.EV3;
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.lcd.TextLCD;
-import lejos.hardware.Sound;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.NXTTouchSensor;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 enum Type {
@@ -18,32 +16,62 @@ enum Type {
 }
 
 public class LejosPiano {
-    public static Server host = new Server();
+
     public static void main(String[] args) throws IOException {
         EV3 ev3 = (EV3) BrickFinder.getLocal();
         TextLCD LCD = ev3.getTextLCD();
-        int buttons = 0;
-        int teller = 0;
-        TrykkSensor s1 = new TrykkSensor(Type.EV3, SensorPort.S1, "A.wav");
-        TrykkSensor s2 = new TrykkSensor(Type.NXT, SensorPort.S2, "C.wav");
-        TrykkSensor s3 = new TrykkSensor(Type.EV3, SensorPort.S3, "F.wav");
-        TrykkSensor s4 = new TrykkSensor(Type.NXT, SensorPort.S4, "G.wav");
 
+        TrykkSensor s1 = new TrykkSensor(Type.EV3, SensorPort.S1);
+        TrykkSensor s2 = new TrykkSensor(Type.NXT, SensorPort.S2);
+        TrykkSensor s3 = new TrykkSensor(Type.EV3, SensorPort.S3);
+        TrykkSensor s4 = new TrykkSensor(Type.NXT, SensorPort.S4);
 
+        Button.UP.addButtonListener();
+
+        Server host = new Server();
         host.connect();
 
+        LCD.clear();
         while(!Button.ESCAPE.isDown()) {
-            if(s1.isPressed()) host.out.writeInt(1);
-            if(s1.isReleased()) host.out.writeInt(2);
+            //PRESSED
+            if(s1.isPressed()) {
+                host.out.writeInt(1);
+                LCD.clear(1);
+                LCD.drawString("S1: Pressed" + "  " + s1.getSample(),0,1);
+            }
+            if(s2.isPressed()) {
+                host.out.writeInt(3);
+                LCD.clear(2);
+                LCD.drawString("S2: Pressed" + "  " + s2.getSample(),0,2);
+            }
+            if(s3.isPressed()) {
+                host.out.writeInt(5);
+                LCD.clear(3);
+                LCD.drawString("S3: Pressed" + "  " + s3.getSample(),0,3);
+            }
+            if(s4.isPressed()) {
+                host.out.writeInt(7);
+                LCD.clear(4);
+                LCD.drawString("S4: Pressed" + "  " + s4.getSample(),0,4);
+            }
 
-            if(s2.isPressed()) host.out.writeInt(3);
-            if(s2.isReleased()); host.out.writeInt(4);
-
-            if(s3.isPressed()) host.out.writeInt(5);
-            if(s3.isReleased()); host.out.writeInt(6);
-
-            if(s4.isPressed()) host.out.writeInt(7);
-            if(s4.isReleased()); host.out.writeInt(8);
+            //RELEASED
+            if(s1.isReleased()) {
+                host.out.writeInt(2);
+                LCD.drawString("S1: Released" + " " + s1.getSample(),0,1);
+            }
+            if(s2.isReleased()) {
+                host.out.writeInt(4);
+                LCD.drawString("S2: Released" + " " + s2.getSample(),0,2);
+            }
+            if(s3.isReleased()) {
+                host.out.writeInt(6);
+                LCD.drawString("S3: Released" + " " + s3.getSample(),0,3);
+            }
+            if(s4.isReleased()) {
+                host.out.writeInt(8);
+                LCD.drawString("S4: Released" + " " + s4.getSample(),0,4);
+            }
         }
     }
     public static void vent() {
@@ -57,17 +85,14 @@ public class LejosPiano {
 
 class TrykkSensor {
     private Port port;
-    private String output;
     private Type type;
     private NXTTouchSensor nxt;
     private EV3TouchSensor ev3;
     private boolean isLifted = true;
-    private boolean isPressed = false;
-    float[] sample;
+    private float[] sample;
 
-    public TrykkSensor(Type type, Port port, String output) {
+    public TrykkSensor(Type type, Port port) {
         this.type = type;
-        this.output = output;
         this.port = port;
 
         if(this.type == Type.NXT) {
@@ -79,17 +104,11 @@ class TrykkSensor {
         this.sample = (this.type == Type.EV3) ? new float[ev3.sampleSize()] : new float[nxt.sampleSize()];
 
     }
-
-    public String toString() {
-        return output;
-    }
-
-
     public boolean isPressed() {
-        if(this.type == Type.EV3) ev3.fetchSample(sample,0);
-        else nxt.fetchSample(sample,0);
+        if(this.type == Type.EV3) ev3.fetchSample(this.sample,0);
+        else nxt.fetchSample(this.sample,0);
 
-        if (isLifted && sample[0] == 1) {
+        if (isLifted && this.sample[0] == 1) {
             isLifted = false;
             return true;
         }
@@ -97,14 +116,13 @@ class TrykkSensor {
         return false;
     }
     public boolean isReleased() {
-        if(this.type == Type.EV3) ev3.fetchSample(sample, 0);
-        else nxt.fetchSample(sample, 0);
-
-        if(!isLifted && sample[0] == 0) {
+        if(!isLifted && this.sample[0] == 0) {
             isLifted = true;
             return true;
         }
-
         return false;
+    }
+    public String getSample() {
+        return Arrays.toString(sample);
     }
 }
