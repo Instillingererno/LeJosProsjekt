e8408e2f08df796f99a724a16d3175e87e54f1cb
Legend K-ern <31963457+cannabuzftw@users.noreply.github.com>
2017-11-20 12:02:56 +0100

    blergh

--- LejosPiano/Spill/src/PianoHero.java
+++ LejosPiano/Spill/src/PianoHero.java
@@ -1,2 +1,72 @@
-public class PianoHero {
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JOptionPane.*;
+import java.util.Random;
+
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
+import com.jogamp.opengl.util.gl2.GLUT;
+
+public class PianoHero extends GLCanvas implements GLEventListener {
+    private static final String title = "PianoHero";
+    private static final int CANVAS_WIDTH = 1500;
+    private static final int CANVAS_HEIGHT = 1500;
+
+    private GLU glu;
+
+    public PianoHero (){this.addGLEventListener(this);}
+
+    public void init (GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        glu = new GLU();
+        gl.glClearColor(0.0f, 0.f, 0.0f, 0.0f);
+        gl.glEnable(GL2.GL_DEPTH_TEST);
+        gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);
+        gl.glShadeModel(GL2.GL_SMOOTH);
+    }
+
+    public void reshape (GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL2 gl = drawable.getGL().getGL2();
+        if (height == 0) height = 1;
+        float aspect = (float) width / (float) height;
+
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45, aspect, 0.1, 10000);
+
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    public void display (GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL2.GL_DEPTH_BUFFER_BIT | GL2.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        gl.glTranslatef(0, 0, -20);
+        gl.glBegin(GL2.GL_TRIANGLES);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glEnd();
+    }
+
+    public void dispose (GLAutoDrawable drawable) {}
+
+    public static void main (String[] args) {
+        GLCanvas canvas = new PianoHero();
+        canvas.setPreferredSize (new Dimension (CANVAS_WIDTH, CANVAS_HEIGHT));
+
+        final JFrame frame = new JFrame();
+        frame.getContentPane().add(canvas);
+        frame.setTitle(title);
+        frame.setDefaultCloseOperation((WindowConstants.EXIT_ON_CLOSE));
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
